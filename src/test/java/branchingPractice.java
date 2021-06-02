9612d5b0e84e16443360902e1db2f3731aca8469
Deum <oscar.sdet@gmail.com>
2021-06-01 23:39:53 -0400

    shared in Githuh

--- src/test/java/branchingPractice.java
+++ src/test/java/branchingPractice.java
@@ -1,7 +1,11 @@
 public class branchingPractice {
     public static void main(String[] args) {
 
-
+//crated new project
+        //enabled version control
+        // added gitignore with idea , target folder and iml file
+        // made initial commit by keeping track of all files
+        // shared project pon github
 
     }
 }
